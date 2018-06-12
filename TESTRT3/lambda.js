b3041062eeac4903d66a19c0856d4fd37ffcb4cb
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-12 14:49:42 +0530

    init

---
+++ TESTRT3/lambda.js
@@ -0,0 +1,13 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.put({
+		TableName: 'TestRT3',
+		Item: { 'itemCode': event.itemCode, 'itemName': event.itemName, 'itemPrice': event.itemPrice, 'itemType': event.itemType }
+	}, function (err, data) {
+		callback(err, data);
+	});
+
+
+	
+}
